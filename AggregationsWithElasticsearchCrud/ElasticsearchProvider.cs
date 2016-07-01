613372561247e551ab9fe96c3be8af353a2624a3
damienbod <damien_bod@hotmail.com>
2016-07-01 11:36:58 +0200

    updating to ElasticsearchCrud.2.3.3.1

--- AggregationsWithElasticsearchCrud/ElasticsearchProvider.cs
+++ AggregationsWithElasticsearchCrud/ElasticsearchProvider.cs
@@ -10,159 +10,161 @@ using ElasticsearchCRUD.Model.SearchModel.Aggregations.RangeParam;
 
 namespace AggregationsWithElasticsearchCrud
 {
-	public class ElasticsearchProvider
-	{
-		protected readonly IElasticsearchMappingResolver ElasticsearchMappingResolver = new ElasticsearchMappingResolver();
-		protected const string ConnectionString = "http://localhost:9200";
+    public class ElasticsearchProvider
+    {
+        protected readonly IElasticsearchMappingResolver ElasticsearchMappingResolver = new ElasticsearchMappingResolver();
+        protected const string ConnectionString = "http://localhost:9200";
 
-		public TermsBucketAggregationsResult SearchFirstNameTermsBucketAggregation()
-		{
-			TermsBucketAggregationsResult aggResult;
-			var search = new Search
-			{
-				Aggs = new List<IAggs>
-				{
-					new TermsBucketAggregation("testFirstName", "firstname")
-					{
-						Size = 20
-					}
-				}
-			};
+        public TermsBucketAggregationsResult SearchFirstNameTermsBucketAggregation()
+        {
+            TermsBucketAggregationsResult aggResult;
+            var search = new Search
+            {
+                Size = 0,
+                Aggs = new List<IAggs>
+                {
+                    new TermsBucketAggregation("testFirstName", "firstname")
+                    {
+                        Size = 20
+                    }
+                }
+            };
 
-			using (var context = new ElasticsearchContext(ConnectionString, ElasticsearchMappingResolver))
-			{
-				var items = context.Search<Person>(search, new SearchUrlParameters { SeachType = SeachType.count });
-				aggResult = items.PayloadResult.Aggregations.GetComplexValue<TermsBucketAggregationsResult>("testFirstName");
-			}
+            using (var context = new ElasticsearchContext(ConnectionString, ElasticsearchMappingResolver))
+            {
+                var items = context.Search<Person>(search, new SearchUrlParameters());
+                aggResult = items.PayloadResult.Aggregations.GetComplexValue<TermsBucketAggregationsResult>("testFirstName");
+            }
 
-			return aggResult;
-		}
+            return aggResult;
+        }
 
-		/// <summary>
-		/// Get the five most popular lastnames groups with the person data
-		/// </summary>
-		/// <returns></returns>
-		public TermsBucketAggregationsResult SearchLastNameTermsBucketAggregations()
-		{
-			TermsBucketAggregationsResult aggResult;
-			var search = new Search
-			{
-				Aggs = new List<IAggs>
-				{
-					new TermsBucketAggregation("testLastName", "lastname")
-					{
-						Size = 5,
-						Aggs = new List<IAggs>
-						{
-							new TopHitsMetricAggregation("tophits")
-							{
-								Size = 2
-							}
-						}
-					}
-				}
-			};
+        /// <summary>
+        /// Get the five most popular lastnames groups with the person data
+        /// </summary>
+        /// <returns></returns>
+        public TermsBucketAggregationsResult SearchLastNameTermsBucketAggregations()
+        {
+            TermsBucketAggregationsResult aggResult;
+            var search = new Search
+            {
+                Aggs = new List<IAggs>
+                {
+                    new TermsBucketAggregation("testLastName", "lastname")
+                    {
+                        Size = 5,
+                        Aggs = new List<IAggs>
+                        {
+                            new TopHitsMetricAggregation("tophits")
+                            {
+                                Size = 2
+                            }
+                        }
+                    }
+                }
+            };
 
-			using (var context = new ElasticsearchContext(ConnectionString, ElasticsearchMappingResolver))
-			{
-				var items = context.Search<Person>(search, new SearchUrlParameters { SeachType = SeachType.count });
-				aggResult = items.PayloadResult.Aggregations.GetComplexValue<TermsBucketAggregationsResult>("testLastName");
-			}
+            using (var context = new ElasticsearchContext(ConnectionString, ElasticsearchMappingResolver))
+            {
+                var items = context.Search<Person>(search, new SearchUrlParameters());
+                aggResult = items.PayloadResult.Aggregations.GetComplexValue<TermsBucketAggregationsResult>("testLastName");
+            }
 
-			return aggResult;
-		}
+            return aggResult;
+        }
 
-		public void SearchLastNameTermsBucketAggregationSignificantTermsBucketAggregationFirstNameMatches()
-		{
-			// Create the aggregation search result
-			TermsBucketAggregationsResult aggResult;
-			var search = new Search
-			{
-				Aggs = new List<IAggs>
-				{
-					new TermsBucketAggregation("testLastName", "lastname")
-					{
-						Size = 0,
-						Aggs = new List<IAggs>
-						{
-							new SignificantTermsBucketAggregation("testFirstName", "firstname")
-							{
-								Size = 20,
-								Aggs = new List<IAggs>
-								{
-									new TopHitsMetricAggregation("tophits")
-									{
-										Size = 20
-									}
-								}
-							}
-						}
-					}
-				}
-			};
+        public void SearchLastNameTermsBucketAggregationSignificantTermsBucketAggregationFirstNameMatches()
+        {
+            // Create the aggregation search result
+            TermsBucketAggregationsResult aggResult;
+            var search = new Search
+            {
+                Aggs = new List<IAggs>
+                {
+                    new TermsBucketAggregation("testLastName", "lastname")
+                    {
+                        Size = 0,
+                        Aggs = new List<IAggs>
+                        {
+                            new SignificantTermsBucketAggregation("testFirstName", "firstname")
+                            {
+                                Size = 20,
+                                Aggs = new List<IAggs>
+                                {
+                                    new TopHitsMetricAggregation("tophits")
+                                    {
+                                        Size = 20
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            };
 
-			// Send the search to Elasticsearch
-			using (var context = new ElasticsearchContext(ConnectionString, ElasticsearchMappingResolver))
-			{
-				var items = context.Search<Person>(search, new SearchUrlParameters { SeachType = SeachType.count });
-				aggResult = items.PayloadResult.Aggregations.GetComplexValue<TermsBucketAggregationsResult>("testLastName");
-			}
+            // Send the search to Elasticsearch
+            using (var context = new ElasticsearchContext(ConnectionString, ElasticsearchMappingResolver))
+            {
+                var items = context.Search<Person>(search, new SearchUrlParameters());
+                aggResult = items.PayloadResult.Aggregations.GetComplexValue<TermsBucketAggregationsResult>("testLastName");
+            }
 
-			// Lets display the Aggregation results in the console
-			foreach (var bucket in aggResult.Buckets)
-			{
-				var significantTermsBucketAggregationsResult = bucket.GetSubAggregationsFromJTokenName<SignificantTermsBucketAggregationsResult>("testFirstName");
+            // Lets display the Aggregation results in the console
+            foreach (var bucket in aggResult.Buckets)
+            {
+                var significantTermsBucketAggregationsResult = bucket.GetSubAggregationsFromJTokenName<SignificantTermsBucketAggregationsResult>("testFirstName");
 
-				foreach (var childbucket in significantTermsBucketAggregationsResult.Buckets)
-				{
-					bool writeHeader = true;
-					var hits = childbucket.GetSubAggregationsFromJTokenName<TopHitsMetricAggregationsResult<Person>>("tophits");
-					foreach (var hit in hits.Hits.HitsResult)
-					{
-						if (writeHeader)
-						{
-							Console.Write("\n{0} {1}, Found Ids: ", hit.Source.FirstName, hit.Source.LastName);
-						}
-						Console.Write("{0} ", hit.Id);
-						writeHeader = false;
-					}
-				}
-			}
-		}
+                foreach (var childbucket in significantTermsBucketAggregationsResult.Buckets)
+                {
+                    bool writeHeader = true;
+                    var hits = childbucket.GetSubAggregationsFromJTokenName<TopHitsMetricAggregationsResult<Person>>("tophits");
+                    foreach (var hit in hits.Hits.HitsResult)
+                    {
+                        if (writeHeader)
+                        {
+                            Console.Write("\n{0} {1}, Found Ids: ", hit.Source.FirstName, hit.Source.LastName);
+                        }
+                        Console.Write("{0} ", hit.Id);
+                        writeHeader = false;
+                    }
+                }
+            }
+        }
 
-		public RangesBucketAggregationsResult SearchAggDateRangesBucketAggregationWithExtentedStats()
-		{
-			RangesBucketAggregationsResult aggResult;
-			var search = new Search
-			{
-				Aggs = new List<IAggs>
-				{
-					new ExtendedStatsMetricAggregation("stats", "modifieddate"),
-					new DateRangeBucketAggregation("testRangesBucketAggregation", "modifieddate", "MM-yyy", new List<RangeAggregationParameter<string>>
-					{
-						new ToRangeAggregationParameter<string>("now-10y/y"),
-						new ToFromRangeAggregationParameter<string>("now-8y/y", "now-9y/y"),
-						new ToFromRangeAggregationParameter<string>("now-7y/y", "now-8y/y"),
-						new ToFromRangeAggregationParameter<string>("now-6y/y", "now-7y/y"),
-						new ToFromRangeAggregationParameter<string>("now-5y/y", "now-6y/y"),
-						new FromRangeAggregationParameter<string>("now-5y/y")
-					})
-					{
-						Aggs = new List<IAggs>
-						{
-							new ExtendedStatsMetricAggregation("stats", "modifieddate")
-						} 
-					}
-				}
-			};
+        public RangesBucketAggregationsResult SearchAggDateRangesBucketAggregationWithExtentedStats()
+        {
+            RangesBucketAggregationsResult aggResult;
+            var search = new Search
+            {
+                Size = 0,
+                Aggs = new List<IAggs>
+                {
+                    new ExtendedStatsMetricAggregation("stats", "modifieddate"),
+                    new DateRangeBucketAggregation("testRangesBucketAggregation", "modifieddate", "MM-yyy", new List<RangeAggregationParameter<string>>
+                    {
+                        new ToRangeAggregationParameter<string>("now-10y/y"),
+                        new ToFromRangeAggregationParameter<string>("now-8y/y", "now-9y/y"),
+                        new ToFromRangeAggregationParameter<string>("now-7y/y", "now-8y/y"),
+                        new ToFromRangeAggregationParameter<string>("now-6y/y", "now-7y/y"),
+                        new ToFromRangeAggregationParameter<string>("now-5y/y", "now-6y/y"),
+                        new FromRangeAggregationParameter<string>("now-5y/y")
+                    })
+                    {
+                        Aggs = new List<IAggs>
+                        {
+                            new ExtendedStatsMetricAggregation("stats", "modifieddate")
+                        } 
+                    }
+                }
+            };
 
-			using (var context = new ElasticsearchContext(ConnectionString, ElasticsearchMappingResolver))
-			{
-				var items = context.Search<Person>(search, new SearchUrlParameters { SeachType = SeachType.count });
-				aggResult = items.PayloadResult.Aggregations.GetComplexValue<RangesBucketAggregationsResult>("testRangesBucketAggregation");
-			}
+            using (var context = new ElasticsearchContext(ConnectionString, ElasticsearchMappingResolver))
+            {
+                var items = context.Search<Person>(search, new SearchUrlParameters());
+                aggResult = items.PayloadResult.Aggregations.GetComplexValue<RangesBucketAggregationsResult>("testRangesBucketAggregation");
+            }
 
-			return aggResult;
-		}
-	}
+            return aggResult;
+        }
+    }
 }
